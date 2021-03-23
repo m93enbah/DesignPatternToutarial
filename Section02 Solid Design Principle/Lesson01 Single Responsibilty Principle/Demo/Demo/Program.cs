ff5ceaac555344abd9abd3683733482c5a0ff773
m93enbah <49711591+m93enbah@users.noreply.github.com>
2021-03-23 15:22:59 +0200

    single responsiblity pattern

---
+++ Section02 Solid Design Principle/Lesson01 Single Responsibilty Principle/Demo/Demo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
+namespace Demo
+{
+
+    public class Jouranl
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count} {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Jouranl j, string fileName, bool overWrite = false)
+        {
+            if (overWrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, j.ToString());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Jouranl();
+            j.AddEntry("Testing");
+            j.AddEntry("Testing2");
+
+            WriteLine(j.ToString());
+
+
+            var p = new Persistence();
+            var fileName = @"D:\\testing.txt";
+            p.SaveToFile(j, fileName, true);
+            Console.ReadLine();
+        }
+    }
+}
