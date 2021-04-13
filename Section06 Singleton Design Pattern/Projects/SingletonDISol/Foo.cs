440bcfeb388305a5df69889f6d4c85b5aa44888a
m.enbeh <mohammedenbah93@gmail.com>
2021-04-13 05:46:57 +0300

    commit

---
+++ Section06 Singleton Design Pattern/Projects/SingletonDISol/Foo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingletonDISol
+{
+    public class Foo
+    {
+        public EventBroker Broker;
+
+        public Foo(EventBroker broker)
+        {
+            Broker = broker ?? throw new ArgumentNullException(paramName: nameof(broker));
+        }
+    }
+
+    public class EventBroker
+    {
+
+    }
+}
---
+++ Section06 Singleton Design Pattern/Projects/SingletonDISol/Program.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using static System.Console;
+
+namespace SingletonDISol
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //it will register EventBroker as singelton and Foo and new instance
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EventBroker>().SingleInstance();
+            builder.RegisterType<Foo>();
+
+            using (var c = builder.Build())
+            {
+                var foo1 = c.Resolve<Foo>();
+                var foo2 = c.Resolve<Foo>();
+                //we see that the foo1 , foo2 is differnt but the broker is singleton so its refer to the same
+                WriteLine(ReferenceEquals(foo1, foo2));
+                WriteLine(ReferenceEquals(foo1.Broker, foo2.Broker));
+            }
+        }
+    }
+}
