51d50cfb7bbd8b12a67ae596c145cd2331e4270c
m93enbah <49711591+m93enbah@users.noreply.github.com>
2021-04-14 14:22:13 +0300

    commit changes

---
+++ Section06 Singleton Design Pattern/Projects/SingletonPatternSol/DoFactorySingeltonPro/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace DoFactorySingeltonPro
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        // Constructor is 'protected' so you cannot create instance directly but through static field
+        protected Singleton(){}
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
