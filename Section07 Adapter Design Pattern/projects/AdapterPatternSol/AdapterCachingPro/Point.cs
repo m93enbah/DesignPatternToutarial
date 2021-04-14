92e39d5892fb6e86aeca6b7e265dafac4885061b
m93enbah <49711591+m93enbah@users.noreply.github.com>
2021-04-14 16:36:54 +0300

    commit

---
+++ Section07 Adapter Design Pattern/projects/AdapterPatternSol/AdapterCachingPro/Point.cs
@@ -0,0 +1,42 @@
+﻿namespace AdapterCachingPro
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        //make check equal by reference if it equals return true
+        //and then check for the two X , Y
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Section07 Adapter Design Pattern/projects/AdapterPatternSol/AdapterCachingPro/Program.cs
@@ -0,0 +1,40 @@
+﻿using MoreLinq;
+using System.Collections.Generic;
+using static System.Console;
+namespace AdapterCachingPro
+{
+    class Program
+    {
+        private static readonly List<VectorObject> vectorObjects = new List<VectorObject>
+    {
+      new VectorRectangle(1, 1, 10, 10),
+      new VectorRectangle(3, 3, 6, 6)
+    };
+
+        // the interface we have
+        public static void DrawPoint(Point p)
+        {
+            Write(".");
+        }
+
+        static void Main(string[] args)
+        {
+            Draw();
+            Draw();
+        }
+
+        private static void Draw()
+        {
+            foreach (var vo in vectorObjects)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ForEach(pt => {
+                        WriteLine($"({pt.X},{pt.Y})");
+                    });
+                }
+            }
+        }
+    }
+}
