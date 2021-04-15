415dd0c31b3d97479f6eb6ade67e588815772ba1
m.enbeh <mohammedenbah93@gmail.com>
2021-04-15 05:12:25 +0300

    asd

---
+++ Section07 Adapter Design Pattern/projects/AdapterPatternSol/DoFactoryAdapter/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+namespace DoFactoryAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Non-adapted chemical compound
+
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            // Wait for user
+
+            ReadKey();
+        }
+    }
+}
